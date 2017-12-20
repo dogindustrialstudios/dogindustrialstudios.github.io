5fe1f6724bbe9e944d0fad72edca16daa1a06823
dogindustrialstudios <34551510+dogindustrialstudios@users.noreply.github.com>
2017-12-19 19:29:11 -0500

    Create index.js

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+function opensidbarNav() {
+    document.getElementById("Sidenav").style.width = "400px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closesidbarNav() {
+    document.getElementById("Sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
