d0d808325e0b77a4f3e013fa5d5595bcc0b14917
dogindustrialstudios <34551510+dogindustrialstudios@users.noreply.github.com>
2017-12-19 19:35:22 -0500

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,12 @@
 function opensidbarNav() {
     document.getElementById("Sidenav").style.width = "400px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
 }
 
 function closesidbarNav() {
     document.getElementById("Sidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
 }
